6a41deb625f1b84874236bedcc23a9b28eade8a8
jakeFromm <jacobfromm@gmail.com>
2020-09-17 16:15:42 -0400

    further progress

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,4 +6,31 @@ class EmployeesController < ApplicationController
     def show
         @employee = Employee.find(params[:id])
     end
+
+    def new 
+        @employee = Employee.new
+      end 
+      
+      def create
+        employee = Employee.create(employee_params)
+        
+        redirect_to employee_path(employee)
+      end 
+    
+      def edit 
+        @employee = Employee.find(params[:id])
+      end 
+      
+      def update 
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        
+        redirect_to employee_path(@employee)
+      end 
+
+      private 
+
+  def employee_params 
+    params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url)
+  end 
 end
